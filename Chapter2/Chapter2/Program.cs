9656bff48dcb8d0b57e3d8b7f703c640e6b87f25
KimBoWoon <haioer@naver.com>
2017-01-05 11:50:39 +0900

    create chapter2

---
+++ Chapter2/Chapter2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(feetToInch(12));
+            Console.WriteLine(feetToInch(100));
+        }
+
+        static int feetToInch(int feet)
+        {
+            return feet * 12;
+        }
+    }
+}
