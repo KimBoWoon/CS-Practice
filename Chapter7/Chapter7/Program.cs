8215c7996bc145ab28ed63f465a835d4a90d8164
KimBoWoon <haioer@naver.com>
2017-01-11 00:04:38 +0900

    chapter4, 7 update

---
+++ Chapter7/Chapter7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Chapter7
+{
+    class Program
+    {
+        static string s = "Hello";
+
+        static void Main(string[] args)
+        {
+            IEnumerator rator = s.GetEnumerator();
+
+            while (rator.MoveNext())
+            {
+                char c = (char)rator.Current;
+                Console.Write(c + ".");
+            }
+        }
+    }
+}
